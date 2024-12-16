90abe10fb67069d212a77f24c98aecc2ef06a7d3
Yasir Hassan <yasirhassan@Hassans-MBP-6.lan>
2024-12-15 18:07:36 -0800

    GUI update - All buttons are functions as they should

--- gui.py
+++ gui.py
@@ -1,7 +1,6 @@
 import os
 import time
-import complete
-import pySimpleGui as sg
+import FreeSimpleGUI as sg
 import functions
 
 if not os.path.exists("processes.txt"):
@@ -17,10 +16,10 @@ clock = sg.Text('', key='clock', size=(20, 1))
 
 label = sg.Text("Open a new process")
 input_box = sg.InputText(tooltip="Open new process", key="process")
-add_button = sg.Button("Add")
+add_button = sg.Button("Open New Process")
 list_box = sg.Listbox(values=functions.get_processes(), key='processes',
                       enable_events=True, size=[45,10])
-edit_button = sg.Button("Edit")
+edit_button = sg.Button("Update")
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
@@ -33,27 +32,27 @@ window = sg.Window('Task Manager',
                            [exit_button]],
                    font=('Helvetica', 20))
 
-
 while True:
-    event, values = window.read(timeout=200)#displays window
-    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    event, values = window.read(timeout=200)  # Displays window
+    print(f"Event triggered: {event}")  # Debugging: log event
+    window["clock"].update(value=time.strftime("%b %d, %Y %I:%M %p"))
+
     match event:
-        case 'Add':
+        case 'Open New Process':
             processes = functions.get_processes()
-            new_processes = values['process'] +"\n"
+            new_processes = values['process'] + "\n"
             processes.append(new_processes)
             functions.write_processes(processes)
             window['processes'].update(values=processes)
 
-
-        case "Edit":
+        case "Update":
             try:
-                todo_edit = values['processes'][0]
-                new_process = values['process'] +"\n"
+                process_edit = values['processes'][0]
+                new_process = values['process'] + "\n"
 
                 processes = functions.get_processes()
                 index = processes.index(process_edit)
-                processes[index] = new_processes
+                processes[index] = new_process
                 functions.write_processes(processes)
                 window['processes'].update(values=processes)
             except IndexError:
@@ -61,12 +60,12 @@ while True:
 
         case "Complete":
             try:
-               process_complete = values['processes'][0]
-               processes = functions.get_processes()
-               processes.remove(process_complete)
-               functions.write_processes(processes)
-               window['processes'].update(values=processes)
-               window['process'].update(value='')
+                process_complete = values['processes'][0]
+                processes = functions.get_processes()
+                processes.remove(process_complete)
+                functions.write_processes(processes)
+                window['processes'].update(values=processes)
+                window['process'].update(value='')
             except IndexError:
                 sg.popup('Please select a process first to complete it.', font=('Helvetica', 20))
 
@@ -76,7 +75,10 @@ while True:
         case 'processes':
             window['process'].update(value=values['processes'][0])
 
-        case sg.WIN_CLOSED: #closes the program when the red close button is pushed
+        case sg.WIN_CLOSED:  # Closes the program when the red close button is pushed
             break
+
+        case _:
+            print(f"Unhandled event: {event}")
 
 window.close()
